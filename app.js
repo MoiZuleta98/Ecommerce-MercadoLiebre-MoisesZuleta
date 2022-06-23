bb15d7673fcd2488134b0e252595f01577ca93d6
MoiZuleta98 <moiseszuleta02@hotmail.com>
2022-06-23 18:18:23 -0300

    Actualizo app para deployar(el app y los scripts del package.json)

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const path = require('path');
 
 const app = express();
 
-app.listen(3001, ()=> console.log("Servidor escuchando en puerto 3001"))
-
-//app.use('./static', express.static(__dirname + './public')) //middelwares
+//middelwares
 app.use(express.static('public'));
 
+//app.listen(3001, ()=> console.log("Servidor escuchando en puerto 3001"))
 //rutas: get, post, put, delete
+
+app.set('puerto',process.env.PORT || 3001)
+
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
 
 app.get('/home', (req, res)=> {
     let htmlPath = path.resolve(__dirname,'./views/home.html')
@@ -25,19 +28,9 @@ app.get('/login', (req,res)=> {
     res.sendFile(htmlPath);
 });
 
-//Cosas que voy a ocupar cuando deploye
-
-//en el app.js
-
-// app.set('puerto',process.env.PORT || 3000)
-
-
-
-// app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
-
 //en el package.json
 
-//"start":"node app",
+// "start":"node app",
 // "dev":"nodemon app.js",
 // "start":"node app",
 // "test": "echo \"Error: no test specified\" && exit 1"
