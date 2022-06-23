1bc56efecf9539c79102454d1a8f319ced514592
MoiZuleta98 <moiseszuleta02@hotmail.com>
2022-06-23 20:54:31 -0300

    cambio app como la del profe

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
 //middelwares
 app.use(express.static('public'));
 
@@ -14,19 +11,17 @@ app.set('puerto',process.env.PORT || 3001)
 app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
 
 app.get('/home', (req, res)=> {
-    let htmlPath = path.resolve(__dirname,'./views/home.html')
-    res.sendFile(htmlPath);
+    res.sendFile(__dirname + './views/home.html');
 });
 
 app.get('/register', (req,res)=> {
-    let htmlPath = path.resolve(__dirname,'./views/register.html');
-    res.sendFile(htmlPath);
+    res.sendFile(__dirname + './views/register.html');
 });
 
 app.get('/login', (req,res)=> {
-    let htmlPath = path.resolve(__dirname,'./views/login.html');
-    res.sendFile(htmlPath);
+    res.sendFile(__dirname + './views/login.html');
 });
+
 
 //en el package.json
 
